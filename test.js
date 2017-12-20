2ea6d2148ad734222527df1ec835dfd9366274b3
lalaina <lala.misa.09@googlemail.com>
2017-12-20 16:00:31 +0300

    update code

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
 module.exports = function(RED) {
     function Test(config) {
         RED.nodes.createNode(this,config);
-        this.server = RED.nodes.getNode(config.amount);        
         var node = this;
         node.on('input', function(msg) {
             msg.payload = msg.payload.toLowerCase();
